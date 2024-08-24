820733481498f7e67df88f0c0b214037b32e2b8f
randradegh <randradedev@gmail.com>
2024-08-23 20:40:42 -0600

    First commit

---
+++ genai_edu_st_01.py
@@ -0,0 +1,152 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="GenAI Edu", 
+    page_icon=None, 
+    layout="wide", 
+    initial_sidebar_state="auto", 
+    menu_items=None
+    )
+
+st.image("header.png", caption="")
+
+"""
+# :blue[Inteligencia Artificial en la Enseñanza Media y Superior] 
+"""
+
+tab1, tab2, tab3, tab4 = st.tabs([':one: Introducción', ":two: Programa", ":three: Horario e Inversión", ":four: Inscripción"])
+
+with tab1:
+    st.header(":one: Introducción")
+    """
+    ### ¡Descubre el Futuro de la Educación con la Inteligencia Artificial!
+
+    ### :book: ¿Eres docente en educación media o superior? ¡Este curso es para ti!
+
+    En un mundo donde la tecnología avanza a pasos agigantados, la Inteligencia Artificial (IA) se ha convertido en una herramienta clave para transformar la educación. ¿Te has preguntado cómo puedes utilizarla en tu enseñanza para mejorar la experiencia de aprendizaje de tus estudiantes?
+
+    Te invitamos a inscribirte en el curso **«Inteligencia Artificial en la Enseñanza Media y Superior»**, diseñado especialmente para profesores como tú, que buscan innovar y llevar su práctica docente al siguiente nivel.
+    
+    ### ¿Por qué debes inscribirte?
+
+    - Actualízate con lo último en tecnología educativa: La IA generativa es una de las tendencias más poderosas en la educación actual. Este curso te brindará las herramientas necesarias para estar a la vanguardia.
+    - Aprende a crear experiencias de aprendizaje personalizadas: Con la IA, podrás adaptar tus clases a las necesidades específicas de tus estudiantes, haciendo que el aprendizaje sea más efectivo y atractivo.
+    - Desarrolla recursos didácticos innovadores: Saldrás del curso con materiales que podrás implementar de inmediato en tus clases.
+
+    ### Modalidad del curso
+
+    El curso se llevará a cabo de manera virtual a través de Google Meet, permitiéndote participar desde la comodidad de tu hogar o lugar de trabajo.
+
+    ### ¡No te quedes fuera!
+
+    La Inteligencia Artificial no es solo el futuro, es el presente de la educación. 
+    
+    Inscríbete ahora y sé parte de la transformación educativa que tus estudiantes necesitan.
+
+    ---
+"""
+    
+
+with tab2:
+    """
+    ## :two: Programa
+
+    En este curso, aprenderás a integrar las herramientas de IA Generativa en tu práctica docente, llevando tus clases al siguiente nivel y preparando a tus estudiantes para los desafíos del siglo XXI.
+
+    - Conoce los conceptos básicos de la tecnología de los grandes modelos de lenguaje (LLMs) y la manera de aprovecharlos para la educación.
+    - Descubre cómo la IA puede revolucionar la creación de contenido educativo, desde guías de estudio hasta simulaciones de laboratorio.
+    - Aprende a utilizar la IA Generativa  para personalizar el aprendizaje, automatizar evaluaciones y desarrollar proyectos prácticos que fomenten el pensamiento crítico y la resolución de problemas.
+    - Crea, de manera automática y en muy poco tiempo, experiencias de aprendizaje personalizadas utilizando ChatGPT y otros modelos de inteligencia artificial generativa
+    Reflexiona sobre las implicaciones éticas de la IA en la educación, asegurando un uso responsable y equitativo de estas tecnologías.
+
+    ### :blue_book: ¿Por qué este curso es para ti?
+
+    Porque la educación del futuro necesita docentes preparados para aprovechar las herramientas tecnológicas que están transformando el mundo. Este curso te dará las habilidades necesarias para innovar en tu enseñanza y maximizar el potencial de tus estudiantes.
+
+    No pierdas esta oportunidad! Inscríbete y sé parte del cambio en la educación. Juntos, podemos hacer que el aprendizaje sea más dinámico, accesible y efectivo.
+
+    ### :compass: Programa del curso
+
+    1. Fundamentos de la IA Generativa:
+
+        1. **Introducción a la IA Generativa y su aplicación en la educación**: Conoce cómo la IA puede revolucionar el aula, facilitando la creación de contenido personalizado y adaptado a las necesidades de cada estudiante.
+
+        2. **Herramientas de IA Generativa**: Descubre las herramientas más avanzadas como ChatGPT y Claude, y aprende a utilizarlas para crear recursos didácticos innovadores.
+
+        3. **Creación de Recursos Didácticos con IA Generativa**: Aprende a diseñar materiales educativos que capten la atención de tus estudiantes y faciliten el aprendizaje
+    
+    2. Implementación Práctica en la Enseñanza:
+
+        1. **Evaluación con IA Generativa**: Optimiza tus evaluaciones y conoce cómo la IA puede ayudarte a medir el progreso de tus estudiantes de manera más precisa.
+
+        2. **Integración en la Planificación de Lecciones**: Descubre cómo integrar la IA en tu planificación diaria para atender mejor la diversidad en el aula.
+
+        3. **Proyectos Prácticos y Desarrollo de Competencias**: Aplica lo aprendido en proyectos concretos que mejoren tus habilidades y las de tus estudiantes.
+
+
+    ### :calendar: Inscríbete ahora y lleva tu enseñanza al siguiente nivel.
+  
+    ---
+    """    
+with tab3:
+    st.header(":three: Fechas y Horario")
+
+    """
+    #### :calendar:  Fechas: 16 al 20 de septiembre de 2024
+
+    #### :clock1: 17 a 20 horas
+
+    #### :hourglass_flowing_sand: Duración: 15 horas
+
+    #### :desktop_computer: Modalidad: a distancia, por Google Meet
+
+    #### :heavy_dollar_sign: Inversión: $1,500 MX (más IVA si se requiere factura)
+
+    ---
+    """
+     
+with tab4:
+    """
+    ## :four: Inscripción:
+
+    Para realizar tu inscripción debes enviar los siguientes datos por correo:
+
+    :book: Nombre completo
+
+    :card_file_box: Grado máximo de estudios
+
+    :book: Nivel en el que ejerces la docencia
+
+    :telephone_receiver: Teléfono (opcional)
+
+    :email: Dirección de correo electrónico
+
+    :derelict_house_building: Ciudad y país de residencia
+
+    :brain: Experiencia en el manejo de la inteligencia artificial generativa
+
+    Pago e Inscripción
+
+    - Depositar $1,500 a la siguiente cuenta:
+        - Roberto Andrade Fonseca
+        - Banco: Banorte
+        - Cuenta: 0015014967
+        - Clabe: 072 180 00015014967 5
+
+    Si requieres factura debes enviar los datos de facturación y se agregará el 16% de IVA al precio del evento.
+    Envía una imagen clara del depósito o transferencia a:
+
+    randradedev@gmail.com 
+    
+    poniendo en el título del mensaje su nombre y el texto ***Pago del curso de GenAI en educación***.
+    Una vez verificado el pago se enviará un mensaje acreditando el mismo.
+    
+    Posteriormente se enviará un correo con los datos para la conexión a la sesión de Meet y algunas indicaciones adicionales sobre el curso.
+
+    #### ¡Te esperamos para explorar juntos cómo la Inteligencia Artificial puede revolucionar tu enseñanza!
+
+    ---
+    """
+"""
+#### #Educación #IA #Docentes #Innovación #TransformaciónDigital #GenAI    
+"""
